61b0ef6570fd1b9e05eafbcc01aacb854eafb462
Jared Wideman <jaredwideman@Jareds-MacBook-Air.local>
2022-07-23 18:43:16 -0400

    tabify main page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,6 +8,11 @@ import {
     Th,
     Td,
     TableContainer,
+    Tabs,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels
   } from '@chakra-ui/react';
 
 import { 
@@ -61,64 +66,79 @@ function Dashboard() {
     player_table_contents.sort((a, b) => b.total - a.total);
 
     return (    
-    <Center>
-    <Box p='6' margin='5px' borderWidth='1px' borderRadius='lg' overflow='hidden' style={{"text-transform": "capitalize"}}>
-    <Center fontWeight='semibold' mt='1' as='h4' marginBottom={'15px'}>Owners</Center>
-    <TableContainer>
-    <Table size='sm' variant='simple'>
-      <Thead>
-        <Tr>
-          <Th><Center>Rank</Center></Th>
-          <Th>Owner</Th>
-          <Th><Center>Fantasy Pts</Center></Th>
-          <Th><Center>Pts Chg</Center></Th>
-          <Th><Center>PB</Center></Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {owner_table_contents.map((content, i) => {
-            return (<Tr>
-                <Td><Center>{i+1}</Center></Td>
-                <Td>{content.owner}</Td>
-                <Td><Center>{content.total.toFixed(1)}</Center></Td>
-                <Td><Center style={content.diff > 0 ? {color: 'green'} : (content.diff < 0 ? {color: 'red'} : {color: 'yellow'})}>{content.diff > 0 ? '+' : ''}{content.diff}</Center></Td>
-                <Td><Center>{(content.total - owner_table_contents[0].total) === 0 ? "-" : -(content.total.toFixed(1) - owner_table_contents[0].total.toFixed(1))}</Center></Td>
-                </Tr>)
-        })}
-      </Tbody>
-    </Table>
-  </TableContainer>
-  </Box>
-  <Box p='6' margin='5px' borderWidth='1px' borderRadius='lg' overflow='hidden' style={{"text-transform": "capitalize"}}>
-    <TableContainer>
-      <Center fontWeight='semibold' mt='1' as='h4' marginBottom={'15px'}>Players</Center>
-    <Table size='sm' variant='simple'>
-      <Thead>
-        <Tr>
-          <Th><Center>Rank</Center></Th>
-          <Th>Player</Th>
-          <Th><Center>Fantasy Pts</Center></Th>
-          <Th><Center>Pts Chg</Center></Th>
-          <Th>B Pts</Th>
-          <Th>P Pts</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {player_table_contents.map((content, i) => {
-            return (<Tr>
-                <Td><Center>{i+1}</Center></Td>
-                <Td>{content.player}</Td>
-                <Td><Center>{content.total.toFixed(1)}</Center></Td>
-                <Td><Center style={content.diff > 0 ? {color: 'green'} : (content.diff < 0 ? {color: 'red'} : {color: 'black'})}>{content.diff > 0 ? '+' : ''}{content.diff}</Center></Td>
-                <Td><Center>{content.batting.toFixed(1)}</Center></Td>
-                <Td><Center>{content.pitching.toFixed(1)}</Center></Td>
-                </Tr>)
-        })}
-      </Tbody>
-    </Table>
-  </TableContainer>
-  </Box>
-  </Center>)
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Owners</Tab>
+          <Tab>Players</Tab>
+        </TabList>
+      <TabPanels>
+          <TabPanel>
+          <Center>
+            <Box p='6' margin='5px' borderWidth='1px' borderRadius='lg' overflow='hidden' maxW="960px" style={{"text-transform": "capitalize"}}>
+            <Center fontWeight='semibold' mt='1' as='h4' marginBottom={'15px'}>Owners</Center>
+            <TableContainer>
+              <Table size='sm' variant='simple'>
+                <Thead>
+                  <Tr>
+                    <Th><Center>Rank</Center></Th>
+                    <Th>Owner</Th>
+                    <Th><Center>Fantasy Pts</Center></Th>
+                    <Th><Center>Pts Chg</Center></Th>
+                    <Th><Center>PB</Center></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {owner_table_contents.map((content, i) => {
+                      return (<Tr>
+                          <Td><Center>{i+1}</Center></Td>
+                          <Td>{content.owner}</Td>
+                          <Td><Center>{content.total.toFixed(1)}</Center></Td>
+                          <Td><Center style={content.diff > 0 ? {color: 'green'} : (content.diff < 0 ? {color: 'red'} : {color: 'yellow'})}>{content.diff > 0 ? '+' : ''}{content.diff}</Center></Td>
+                          <Td><Center>{(content.total - owner_table_contents[0].total) === 0 ? "-" : -(content.total.toFixed(1) - owner_table_contents[0].total.toFixed(1))}</Center></Td>
+                          </Tr>)
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+          </Center>
+          </TabPanel>
+          <TabPanel>
+          <Center>
+            <Box p='6' margin='5px' borderWidth='1px' borderRadius='lg' overflow='hidden' maxW="960px" style={{"text-transform": "capitalize"}}>
+            <TableContainer>
+              <Center fontWeight='semibold' mt='1' as='h4' marginBottom={'15px'}>Players</Center>
+              <Table size='sm' variant='simple'>
+                <Thead>
+                  <Tr>
+                    <Th><Center>Rank</Center></Th>
+                    <Th>Player</Th>
+                    <Th><Center>Fantasy Pts</Center></Th>
+                    <Th><Center>Pts Chg</Center></Th>
+                    <Th>B Pts</Th>
+                    <Th>P Pts</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {player_table_contents.map((content, i) => {
+                      return (<Tr>
+                          <Td><Center>{i+1}</Center></Td>
+                          <Td>{content.player}</Td>
+                          <Td><Center>{content.total.toFixed(1)}</Center></Td>
+                          <Td><Center style={content.diff > 0 ? {color: 'green'} : (content.diff < 0 ? {color: 'red'} : {color: 'black'})}>{content.diff > 0 ? '+' : ''}{content.diff}</Center></Td>
+                          <Td><Center>{content.batting.toFixed(1)}</Center></Td>
+                          <Td><Center>{content.pitching.toFixed(1)}</Center></Td>
+                          </Tr>)
+                  })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            </Box>
+          </Center>
+          </TabPanel>
+      </TabPanels>
+      </Tabs>
+  )
 }
 
 export default Dashboard;
